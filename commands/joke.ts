8bd3091c40f8d41d972dd239d65b8bf2a41e3a13
Hamziniii <hamzagaziuddin04@gmail.com>
2021-09-02 09:50:36 -0500

    added a boolean option: private

--- commands/joke.ts
+++ commands/joke.ts
@@ -5,10 +5,10 @@ import { Machi } from "../machina"
 import axios from 'axios'
 
 export const joke: Machi = {
-    data: (new SlashCommandBuilder()).setDescription("lets go"),
+    data: (new SlashCommandBuilder()).setDescription("Tells a joke!").addBooleanOption(option => option.setName('private').setDescription('Should this only be sent to you?').setRequired(false)),
     execute: async (interaction: CommandInteraction) => {
         let res = await axios.get('https://official-joke-api.appspot.com/random_joke')
-        interaction.reply(`${res.data.setup} \n||${res.data.punchline}||`)
+        interaction.reply({content: `${res.data.setup} \n||${res.data.punchline}||`, ephemeral: interaction.options.getBoolean('private')})
     },
     inDev: false 
 }
